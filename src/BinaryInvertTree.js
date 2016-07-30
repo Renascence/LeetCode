4a1aab85175057333fb09b7179b7f5d753262c99
Renascence <863692900@qq.com>
2016-07-30 23:34:50 +0800

    反转二叉树

--- src/BinaryInvertTree.js
+++ src/BinaryInvertTree.js
@@ -12,10 +12,8 @@
  * @return {TreeNode}
  */
 var invertTree = function(root) {
-	if (root == null ) return root
-  	if(root.left == null && root.right == null)	return root
-
-	var temp = root.right || null
+	if (root == null || (root.left == null && root.right == null)) return root
+	var temp = root.right
 	root.right = root.left
 	root.left = temp
    	invertTree(root.right)
