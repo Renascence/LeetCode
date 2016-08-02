298a52c03bf3de6adb3ac789fc2d42b2661086bf
Renascence <863692900@qq.com>
2016-08-02 21:13:44 +0800

    finish move zeros

---
+++ src/MoveZeroes.js
@@ -0,0 +1,17 @@
+// Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+// For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0].
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+	var len = nums.length
+  for(var j=len-1; j >= 0 ; j --){
+  	if(!nums[j]){
+  		nums.splice(j,1)
+  		nums[len-1] = 0
+  	}
+  }
+};
