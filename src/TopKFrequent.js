647b1aff16318a7de6d5aa031ec6c5b95f2d5ddb
Renascence <863692900@qq.com>
2016-08-04 20:35:21 +0800

    modify Given a non-empty array of integers, return the k most frequent elements.

--- src/TopKFrequent.js
+++ src/TopKFrequent.js
@@ -1,32 +1,30 @@
+// Given a non-empty array of integers, return the k most frequent elements.
+
+// For example,
+// Given [1,1,1,2,2,3] and k = 2, return [1,2].
+
 /**
 * @param {number[]} nums
 * @param {number} k
 * @return {number[]}
 */
 var topKFrequent = function(nums, k) {
-	if(nums.length == k){
+    var len = nums.length
+	if(len == k){
 		return nums
 	}
 	nums = nums.sort()
-	var cut = 1, c = [],v = [],res = [],len = nums.length
+	var obj = []
+	var cut = 1,res = []
 	for(var i=len-1;i>=1;i--){
-		if(nums[i] == nums[i-1]){
-			if(i==1){
-				c.push(cut +1)
-				v.push(nums[i])
-			}
+		if(nums[i] == nums[i-1] && i != 1){
 			nums.splice(i,1)
 			cut ++
 		}
 		else{
-			c.push(cut)
-			v.push(nums[i])
+			i==1 ? obj.push([cut +1,nums[i]]) : obj.push([cut,nums[i]])
 			cut = 1
 		}
-	}
-	var obj = []
-	for(var i =0 ; i < c.length ; i++){
-		obj.push([c[i],v[i]])
 	}
 	obj.sort(function(a, b){return b[0] - a[0]});  
 	for(var j = 0 ; j < k ; j ++){
