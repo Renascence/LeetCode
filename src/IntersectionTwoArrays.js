bb8b949e800065b16135a835ad22073c7a338340
Renascence <863692900@qq.com>
2016-08-05 20:34:44 +0800

    IntersectionTwoArrays.js

---
+++ src/IntersectionTwoArrays.js
@@ -0,0 +1,16 @@
+// Given two arrays, write a function to compute their intersection.
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+	var res = []
+  for(i in nums1){
+  	if(nums2.indexOf(nums1[i]) != -1 && res.indexOf(nums1[i]) == -1){
+  		res.push(nums1[i])
+  	}
+  }
+  return res  
+};
