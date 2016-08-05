b87c8bee9ceab3a5bd231c4e2a2d4a61c125e984
Renascence <863692900@qq.com>
2016-08-05 20:17:25 +0800

    ProductofArrayExceptSelf.js

---
+++ src/ProductofArrayExceptSelf.js
@@ -0,0 +1,42 @@
+// Given an array of n integers where n > 1, nums, return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+
+// Solve it without division and in O(n).
+
+// For example, given [1,2,3,4], return [24,12,8,6].
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    var he = 1
+    var res = []
+    var mark
+    var count = 0
+    for(var i =0 ; i < nums.length ; i++){
+        if(nums[i]!=0){
+        he *= nums[i]
+        }
+        else{
+            count ++
+            mark = i
+        }
+    }
+    if(count == 1){
+        for(b in nums){
+            res[b] = 0
+        }
+        res[mark] = he;
+        return res
+    }
+    if(count >= 2){
+        for(a in nums){
+            res[a] = 0
+        }
+        return res
+    }
+    for(var j =0 ; j < nums.length ; j++){
+        nums[j] == 0 ? res.push(he) : res.push(he/nums[j])
+    }
+    return res
+};
