7ef4e8c7143ea7cda3cc994171a54c14fad76f3a
Renascence <863692900@qq.com>
2016-08-18 21:57:01 +0800

    ReverseLinkedList.js

---
+++ src/ReverseLinkedList.js
@@ -0,0 +1,29 @@
+// Reverse a singly linked list.
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+ var reverseList = function(head) {
+ 	if (!head) {
+ 		return []
+ 	}
+ 	var cur = head
+ 	var ol = null
+ 	var ne = null
+ 	while (cur) {
+ 		ol = cur.next
+ 		cur.next = ne
+ 		ne = cur
+ 		cur = ol
+ 	}
+ 	return ne
+ };
+
