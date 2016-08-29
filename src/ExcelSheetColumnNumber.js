3960f0cd48ea99ac5ae7b2de2b0aa1267d3709cd
Renascence <863692900@qq.com>
2016-08-29 22:43:10 +0800

    ExcelSheetColumnNumber.js

---
+++ src/ExcelSheetColumnNumber.js
@@ -0,0 +1,18 @@
+// Related to question Excel Sheet Column Title
+
+// Given a column title as appear in an Excel sheet, return its corresponding column number.
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var titleToNumber = function(s) {
+    var res = 0;
+    var len = s.length;
+    for (var i = 0; i < len ; i ++) {
+    	res = res * 26 + s.charCodeAt(i) - 64
+    }
+    return res;
+};
+
+console.log(titleToNumber('AC'));
