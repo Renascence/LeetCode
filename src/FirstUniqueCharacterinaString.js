cacc0643295283e12c98bf513ac3ceb7d38887f3
Renascence <863692900@qq.com>
2016-08-30 21:19:16 +0800

    FirstUniqueCharacterinaString.js

---
+++ src/FirstUniqueCharacterinaString.js
@@ -0,0 +1,18 @@
+// Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+    var len = s.length
+    if(len == 1) {
+    	return 0
+    }
+    for (var i = 0; i < len - 1 ; i++) {
+    	if(s.indexOf(s[i])==i && s.indexOf(s[i],i+1)==-1){
+    		return i
+    	}
+    }
+    return -1
+};
