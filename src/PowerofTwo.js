383ed8e5970f8d2065d41cbd604c84b3059ef39d
Renascence <863692900@qq.com>
2016-08-31 23:38:17 +0800

    PowerofTwo.js

---
+++ src/PowerofTwo.js
@@ -0,0 +1,13 @@
+// Given an integer, write a function to determine if it is a power of two.
+
+
+
+var isPowerOfTwo = function(n) {
+  if(n==1) {
+    return true
+  }
+  while (n>2) {
+    n=n/2
+  }
+  return n==2 ? true : false
+};
