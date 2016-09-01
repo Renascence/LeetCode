8cc1191f6db56640d6f8a252a32b805ff1321aaa
Renascence <863692900@qq.com>
2016-09-01 21:57:34 +0800

    PowerofThree.js

---
+++ src/PowerofThree.js
@@ -0,0 +1,12 @@
+// Given an integer, write a function to determine if it is a power of three.
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+    while (n>1) {
+      n = n / 3
+    }
+    return n==1 ? true : false
+};
