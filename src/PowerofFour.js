e50fb4babf5f56ad286151b969e9faa14db0ecfa
Renascence <863692900@qq.com>
2016-09-03 22:07:59 +0800

    PowerofFour.js

---
+++ src/PowerofFour.js
@@ -0,0 +1,13 @@
+// Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
+
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPowerOfFour = function(num) {
+    if(num == 1) return true
+    while(num>4){
+        num = num / 4
+    }
+    return num==4 ? true:false
+};
