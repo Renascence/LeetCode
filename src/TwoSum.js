cce405d5d3c7e30fd6b5561d981fc336749b98a5
Renascence <863692900@qq.com>
2016-09-08 22:53:34 +0800

    TwoSum.js

--- src/TwoSum.js
+++ src/TwoSum.js
@@ -9,9 +9,8 @@
  */
 var twoSum = function(nums, target) {
     for (var i = 0; i < nums.length; i++) {
-    	console.log(target- nums[i]);
     	if(nums.indexOf(target - nums[i],i+1) != -1){
-    		return [i,nums.indexOf(target - nums[i])]
+    		return [i,nums.indexOf(target - nums[i],i+1)]
     	}
     }
 };
