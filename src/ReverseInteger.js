c093b6fcaad6b05f73b7f27bdfd7d920d2acd1bd
Renascence <863692900@qq.com>
2016-09-14 23:55:56 +0800

    Reverse Integer

---
+++ src/ReverseInteger.js
@@ -0,0 +1,20 @@
+// Reverse digits of an integer.
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+	x = x + ''
+	if(x.length == 1) return parseInt(x)
+	var flag = ''
+ 	if(x[0] == '-') {
+		flag = '-'
+		x = x.substring(1)
+	}
+	x=x.split('').reverse().join('')
+	while(x[0] == 0){
+		x = x.substring(1)
+	}
+	return parseInt(flag+x)
+};
