cc1d3c684b000462f6b2716a271cd5be5fc16218
Renascence <geniusglory@outlook.com>
2016-09-27 21:38:47 +0800

    Sum of Left Leaves

---
+++ src/SumofLeftLeaves.js
@@ -0,0 +1,34 @@
+// Find the sum of all left leaves in a given binary tree.
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumOfLeftLeaves = function (root) {
+  if (root == null) return 0
+  var sum = 0
+  cal(root, root.left)
+  cal(root, root.right)
+  return sum
+
+  function cal(fat, cur) {
+    if (fat == null || cur == null) return
+    if (cur.left == null && cur.right == null) {
+
+      if (fat.left == cur) {
+        sum += cur.val
+      }
+      return
+    }
+    if (cur.left) cal(cur, cur.left)
+    if (cur.right) cal(cur, cur.right)
+
+  }
+};
