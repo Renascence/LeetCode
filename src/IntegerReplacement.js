3ecfabd6e0f4f3f7857b21ed5e2d6e98f6c5e9ad
Renascence <geniusglory@outlook.com>
2016-09-30 23:42:27 +0800

    Integer Replacement

---
+++ src/IntegerReplacement.js
@@ -0,0 +1,16 @@
+// Given a positive integer n and you can do operations as follow:
+
+// If n is even, replace n with n/2.
+// If n is odd, you can replace n with either n + 1 or n - 1.
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var integerReplacement = function (n) {
+  if (n == 1) return 0
+  if (n % 2 == 0) return 1 + integerReplacement(n / 2)
+  else {
+    return 2 + Math.min(integerReplacement((n + 1) / 2), integerReplacement((n - 1) / 2))
+  }
+};
