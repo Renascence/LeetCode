771bf6befa289d019c92ed0f0c2e09d4a17d918d
Renascence <geniusglory@outlook.com>
2016-10-13 22:26:03 +0800

    Factorial Trailing Zeroes

---
+++ src/FactorialTrailingZeroes.js
@@ -0,0 +1,14 @@
+// Given an integer n, return the number of trailing zeroes in n!.
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var trailingZeroes = function (n) {
+  var res = 0
+  while (n / 5 != 0) {
+    n = parseInt(n/5)
+    res = res + n
+  }
+  return res
+};
