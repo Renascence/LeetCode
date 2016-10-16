8f7d651f34b0b23fcb18eef7ec50ba2ada0ec9b2
Renascence <geniusglory@outlook.com>
2016-10-16 21:21:24 +0800

    Lowest Common Ancestor of a Binary Search Tree

---
+++ src/LowestCommonAncestorofaBinarySearchTree.js
@@ -0,0 +1,21 @@
+// Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+  if(!root || root == p || root == q) return root
+  if(Math.max(q.val,p.val) < root.val) return lowestCommonAncestor(root.left,p,q)
+  else if(Math.min(q.val,p.val) > root.val) return lowestCommonAncestor(root.right,p,q)
+  else return root
+};
