898740b887dc03d12c4eaaed705691758f0664cf
Renascence <geniusglory@outlook.com>
2016-10-30 22:44:40 +0800

    Rotate Array

---
+++ src/RotateArray.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function (nums, k) {
+  k = k % nums.length
+  k = nums.length - k
+  var temp = nums.splice(0, k)
+  for (var i = 0; i < temp.length; i++) {
+    nums.push(temp[i])
+  }
+};
