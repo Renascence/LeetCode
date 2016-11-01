b9f444df072fa8bef9fd2978bacfd0a5caf73da0
Renascence <geniusglory@outlook.com>
2016-11-02 00:04:14 +0800

    Compare Version Numbers

--- src/CompareVersionNumbers.js
+++ src/CompareVersionNumbers.js
@@ -12,18 +12,38 @@ var compareVersion = function (version1, version2) {
   }
   var num1 = version1.substring(log - 1).split('.')
   var num2 = version2.substring(log - 1).split('.')
-  var flag = 0
-  for (var i = 0; i < num1.length; i++) {
-    console.log(num1[i],num2[i])
-    if(num2[i] == undefined) num2[i] =0
-    if (parseInt(num1[i]) == parseInt(num2[i])) flag ++
-    
+  if(num1.length > num2.length) {
+    var flag = 0 
+    for (var i = 0; i < num1.length; i++) {
+      if(num2[i] == undefined) num2[i] =0
+      if (parseInt(num1[i]) == parseInt(num2[i])) flag ++
+      
+    }
+    if(flag == num1.length) return 0
+    for (var i = 0; i < num1.length; i++) {
+      if(num2[i] == undefined) num2[i] =0
+      
+      if (parseInt(num1[i]) > parseInt(num2[i])) return 1
+      if (parseInt(num1[i]) < parseInt(num2[i])) return -1
+      
+    }
+    return -1
   }
-  if(flag == num1.length) return 0
-  for (var i = 0; i < num1.length; i++) {
-    if(num2[i] == undefined) num2[i] =0
-    
-    if (parseInt(num1[i]) > parseInt(num2[i])) return 1
+  else {
+    var flag = 0 
+    for (var i = 0; i < num2.length; i++) {
+      if(num1[i] == undefined) num1[i] =0
+      if (parseInt(num1[i]) == parseInt(num2[i])) flag ++
+      
+    }
+    if(flag == num2.length) return 0
+    for (var i = 0; i < num2.length; i++) {
+      if(num1[i] == undefined) num1[i] =0
+      
+      if (parseInt(num1[i]) > parseInt(num2[i])) return 1
+      if (parseInt(num1[i]) < parseInt(num2[i])) return -1
+      
+    }
+    return -1
   }
-  return -1
 };
