7263d8e68bb22f678ace4473ebcbbad1901a0ee0
Renascence <geniusglory@outlook.com>
2016-11-07 22:20:07 +0800

    Minimum Moves to Equal Array Elements

---
+++ src/MinimumMovestoEqualArrayElements.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minMoves = function (nums) {
+  if (nums.length == 1) return 0
+  var min = nums[0]
+  var len = nums.length
+  var sum = 0
+  for (var i = 0; i < len; i++) {
+    if (nums[i] < min) min = nums[i]
+    sum += nums[i]
+  }
+  return sum - min*len
+};
