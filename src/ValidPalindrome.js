5eb4376e73ebbd4bcdf48da6e9f44c01556d1894
Renascence <863692900@qq.com>
2016-11-09 22:20:56 +0800

    Valid Palindrome

---
+++ src/ValidPalindrome.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+	s=s.toLowerCase()
+	var str = ''
+	for (var i = 0; i < s.length; i++) {
+		if(/^[a-zA-Z0-9]*$/.test(s[i]))
+				str += s[i]
+	}
+	return str == str.split('').reverse().join('')
+};
