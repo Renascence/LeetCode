01e26fe1d008b8e54b02a5b8a3f8f91b5ab83ca0
Renascence <geniusglory@outlook.com>
2016-11-17 22:15:55 +0800

    Assign Cookies

---
+++ src/AssignCookies.js
@@ -0,0 +1,20 @@
+var findContentChildren = function (g, s) {
+  var child = g.sort(function (a, b) {
+    return a - b
+  })
+  var cookie = s.sort(function (a, b) {
+    return a - b
+  })
+  var i = 0, j = 0, res = 0
+  while (child[j] && cookie[i]) {
+    if (child[j] <= cookie[i]) {
+      res++
+      i++
+      j++
+    }
+    else {
+      i++
+    }
+  }
+  return res
+};
