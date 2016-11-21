daa23979ea5468b50469e26e3c5f9c5a9169dc8d
Renascence <geniusglory@outlook.com>
2016-11-21 22:01:56 +0800

    Island Perimeter

---
+++ src/IslandPerimeter.js
@@ -0,0 +1,29 @@
+var islandPerimeter = function (grid) {
+  var interfacing = 0
+  var nums = 0
+  for (var i = 0; i < grid.length; i++) {
+    for (var j = 0; j < grid[0].length; j++) {
+      interfacing += cal(i, j)
+      if (grid[i][j] == 1) nums++
+    }
+  }
+  return nums * 4 - interfacing
+  function cal(i, j) {
+    var len = 0
+    if (grid[i][j] == 1) {
+      if (i > 0) {
+        if (grid[i - 1][j] == 1) len++
+      }
+      if (i < grid.length - 1) {
+        if (grid[i + 1][j] == 1) len++
+      }
+      if (j > 0) {
+        if (grid[i][j - 1] == 1) len++
+      }
+      if (j < grid[0].length - 1) {
+        if (grid[i][j + 1] == 1) len++
+      }
+    }
+    return len
+  }
+};
