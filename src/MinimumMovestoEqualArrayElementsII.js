69fa7c662620f9c2f9677e38973f8f287f2a5f14
Renascence <863692900@qq.com>
2016-11-25 19:46:56 +0800

    Minimum Moves to Equal Array Elements II

---
+++ src/MinimumMovestoEqualArrayElementsII.js
@@ -0,0 +1,11 @@
+var minMoves2 = function (nums) {
+  nums.sort(function (a, b) {
+    return a - b
+  })
+  var ave = nums[parseInt(nums.length / 2)]
+  var res = 0
+  for (var i in nums) {
+    res = Math.abs(nums[i] - ave) + res
+  }
+  return res
+};
