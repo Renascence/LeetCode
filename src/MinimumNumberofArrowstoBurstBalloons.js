4a1f1bb25cd09f205496fa2a26b83184760fbbdb
Renascence <geniusglory@outlook.com>
2016-11-30 21:35:32 +0800

    Minimum Number of Arrows to Burst Balloons

---
+++ src/MinimumNumberofArrowstoBurstBalloons.js
@@ -0,0 +1,18 @@
+var findMinArrowShots = function (points) {
+  if (points.length == 0) return 0
+  points.sort(function (a, b) {
+    return a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]
+  });
+  var res = 1
+  var end = points[0][1]
+  for (var i in points) {
+    if (end >= points[i][0]) {
+      end = Math.min(end, points[i][1])
+    }
+    else {
+      res++
+      end = points[i][1]
+    }
+  }
+  return res
+};
