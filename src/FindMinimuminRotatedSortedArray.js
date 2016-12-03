2fd7d6832039c3276403631e50f4c6939ffc3e0b
Renascence <863692900@qq.com>
2016-12-03 23:18:07 +0800

    Find Minimum in Rotated Sorted Array

---
+++ src/FindMinimuminRotatedSortedArray.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function (nums) {
+  if (nums.length == 1) return nums[0]
+  var left = 0
+  var right = nums.length - 1
+  if (nums[left] < nums[right]) return nums[0]
+  while (left < right) {
+    var mid = parseInt((left + right) / 2)
+    if (left == right - 1) return nums[right]
+    if (nums[left] <= nums[mid]) {
+      left = mid
+    } else {
+      right = mid
+    }
+  }
+};
