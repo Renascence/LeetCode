0b7d540b6b99c30aa96ad330f504b5a072913d28
Renascence <863692900@qq.com>
2016-12-22 23:17:58 +0800

    Hamming Distance

---
+++ src/HammingDistance.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} x
+ * @param {number} y
+ * @return {number}
+ */
+var hammingDistance = function (x, y) {
+  var num = x ^ y
+  var res = 0
+  var bin = num.toString(2)
+  for (var i in bin) {
+    if (bin[i] == 1) res++
+  }
+  return res
+};
