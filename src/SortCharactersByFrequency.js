95bb867348c7dc6208091dad564b0a4a5b905d54
Renascence <geniusglory@outlook.com>
2016-12-27 23:52:25 +0800

    Sort Characters By Frequency

---
+++ src/SortCharactersByFrequency.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var frequencySort = function (s) {
+  var hash = []
+  for (var i in s) {
+    if (!hash[s[i].charCodeAt()]) {
+      hash[s[i].charCodeAt()] = s[i]
+    } else {
+      hash[s[i].charCodeAt()] += s[i]
+    }
+  }
+  hash.sort(function (a, b) {
+    return b.length - a.length
+  })
+  return hash.join('')
+};
