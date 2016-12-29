1c2302bf200486f4e6a5c7c2262aa9370690d8c8
Renascence <863692900@qq.com>
2016-12-29 22:59:50 +0800

    4Sum II

---
+++ src/4SumII.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} A
+ * @param {number[]} B
+ * @param {number[]} C
+ * @param {number[]} D
+ * @return {number}
+ */
+var fourSumCount = function (A, B, C, D) {
+  var len = A.length
+  var sumAB = {}, sumCD = {}, res = 0
+  for (var i = 0; i < len; i++) {
+    for (var j = 0; j < len; j++) {
+      if (sumAB[A[i] + B[j]]) {
+        sumAB[A[i] + B[j]]++
+      } else {
+        sumAB[A[i] + B[j]] = 1
+      }
+      if (sumCD[C[i] + D[j]]) {
+        sumCD[C[i] + D[j]]++
+      } else {
+        sumCD[C[i] + D[j]] = 1
+      }
+    }
+  }
+  for (var k in sumAB) {
+    if (sumCD[k * -1]) {
+      res += sumAB[k] * sumCD[k * -1]
+    }
+  }
+  return res
+};
