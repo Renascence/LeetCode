a2478ec3a77073e5e473932adb6dca702fe665d0
Renascence <863692900@qq.com>
2016-12-31 23:41:13 +0800

    Generate Parentheses

---
+++ src/GenerateParentheses.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function (n) {
+  var res = []
+  function rec(left, right, str) {
+    if (left == 0 && right == 0) {
+      console.log(str)
+      res.push(str)
+    }
+    if (left > 0) {
+      rec(left - 1, right, str + '(')
+    }
+    if (right > 0 && left < right) {
+      rec(left, right - 1, str + ')')
+    }
+  }
+  rec(n, n, '')
+  return res
+};
