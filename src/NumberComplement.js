d4654d901a8ead3799e9347086a6f33963d0db99
Renascence <863692900@qq.com>
2017-01-14 21:21:13 +0800

    Number Complement

---
+++ src/NumberComplement.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var findComplement = function (num) {
+  var temp = num
+  var mask = 1
+  while (temp > 0) {
+    mask = mask << 1
+    temp = temp >> 1
+  }
+  return num ^ (mask - 1)
+};
