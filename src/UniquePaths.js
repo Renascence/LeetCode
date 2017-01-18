befb373abb41195d90320cf948d666a908fa4d2d
Renascence <863692900@qq.com>
2017-01-18 23:14:38 +0800

    Unique Paths

---
+++ src/UniquePaths.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function (m, n) {
+  var map = []
+  for (var i = 0; i < m; i++) map[i] = []
+  for (var i = 0; i < m; i++) {
+    for (var j = 0; j < n; j++) {
+      if (i == 0 && j == 0) map[i][j] = 1
+      else if (i == 0) map[i][j] = 1
+      else if (j == 0) map[i][j] = 1
+      else map[i][j] = map[i][j - 1] + map[i - 1][j]
+    }
+  }
+  return map[m - 1][n - 1]
+};
