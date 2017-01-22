55fe9c6260b32d800f23b04568efa150d82ce4d3
Renascence <geniusglory@outlook.com>
2017-01-22 20:27:34 +0800

    Construct the Rectangle

--- ConstructtheRectangle.js
+++ ConstructtheRectangle.js
@@ -1,1 +1,13 @@
-gss
+/**
+ * @param {number} area
+ * @return {number[]}
+ */
+var constructRectangle = function (area) {
+  var w = area;
+  var res = [area,1]
+  while(w >= area/w) {
+    if(area%w == 0) res=[w,area/w]
+    w--
+  }
+  return res
+};
