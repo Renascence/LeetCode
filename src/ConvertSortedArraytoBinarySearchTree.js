1b28363b6afa038524d4a769236de915320d6f29
Renascence <geniusglory@outlook.com>
2017-01-28 22:47:04 +0800

    Convert Sorted Array to Binary Search Tree

---
+++ src/ConvertSortedArraytoBinarySearchTree.js
@@ -0,0 +1,13 @@
+var sortedArrayToBST = function (nums) {
+  function build(left, right) {
+    if (left > right) return null
+    var mid = parseInt((left + right) / 2)
+    var node = new TreeNode(nums[mid])
+    node.left = build(left, mid - 1)
+    node.right = build(mid + 1, right)
+    return node
+  }
+  var len = nums.length
+
+  return build(0, len - 1)
+};
