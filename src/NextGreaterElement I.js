105f949c9e218ae76ebff1954b3cde63a5df3fc4
Renascence <863692900@qq.com>
2017-02-11 23:01:18 +0800

    Next Greater Element I

---
+++ src/NextGreaterElement I.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} findNums
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var nextGreaterElement = function (findNums, nums) {
+  var len = nums.length
+  var res = []
+  for (var i in findNums) {
+    var pos = nums.indexOf(findNums[i])
+    var flag = false
+    for (var j = pos + 1; j < len; j++) {
+      if (nums[j] > findNums[i]) {
+        res.push(nums[j])
+        flag = true
+        break
+      }
+    }
+    if (!flag) res.push(-1)
+  }
+  return res
+};
