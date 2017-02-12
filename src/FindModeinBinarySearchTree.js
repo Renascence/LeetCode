300d8f0161c9a1bb9bfc14955344f5660f9ac55e
Renascence <863692900@qq.com>
2017-02-12 23:04:33 +0800

    Find Mode in Binary Search Tree

---
+++ src/FindModeinBinarySearchTree.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var findMode = function (root) {
+  if(!root) return []
+  var vals = []
+  var obj = {}
+  var max = 0, times = 0,res = [0]
+  function order(root) {
+    if (root == null) return
+    vals.push(root.val)
+    order(root.left)
+    order(root.right)
+  }
+  order(root)
+  for (var i in vals) {
+    if (obj[vals[i]]) obj[vals[i]]++
+    else obj[vals[i]] = 1
+  }
+  for (var j in obj) {
+    if(obj[j] > times) {
+      res.length = 0
+      times = obj[j]
+      res.push(parseInt(j))
+    } else if(obj[j] == times) {
+      res.push(parseInt(j))
+    }
+  }
+  return res
+};
