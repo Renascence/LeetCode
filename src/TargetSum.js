65c9d13fa81faff69934ceda821b0d523f0754dd
Renascence <geniusglory@outlook.com>
2017-03-15 00:47:07 +0800

    Target Sum

--- src/TargetSum.js
+++ src/TargetSum.js
@@ -4,16 +4,18 @@
  * @return {number}
  */
 var findTargetSumWays = function (nums, S) {
-  var res = 0
-  function map(nums, sum, lv) {
-    if (lv == nums.length) {
-      if (sum == S) res++
-      return
+  function map(nums, sum, lv, hash) {
+    const key = `${lv}->${sum}`
+    if (hash.hasOwnProperty(key)) {
+      return hash[key]
     }
-    if (lv > nums.length) return
-    map(nums, sum + nums[lv], lv + 1)
-    map(nums, sum - nums[lv], lv + 1)
+    if (lv == nums.length) {
+      return sum === S ? 1 : 0
+    }
+    let add = map(nums, sum + nums[lv], lv + 1, hash)
+    let minus = map(nums, sum - nums[lv], lv + 1, hash)
+    hash[key] = add + minus
+    return add+minus
   }
-  map(nums, 0, 0)
-  return res
+  return map(nums, 0, 0, {})
 };
