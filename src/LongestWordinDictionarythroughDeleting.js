8ee33061d9f1e0f597ee534b1a3da783103ff72f
Renascence <geniusglory@outlook.com>
2017-03-22 23:59:07 +0800

    Longest Word in Dictionary through Deleting

---
+++ src/LongestWordinDictionarythroughDeleting.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @param {string[]} d
+ * @return {string}
+ */
+var findLongestWord = function (s, d) {
+  let res = ""
+  for (let i = 0; i < d.length; i++) {
+    if (d[i] < res.length) {
+      continue
+    }
+    if (findWord(d[i], s)) {
+      if (d[i].length > res.length) res = d[i]
+      else if (d[i].length == res.length && d[i].charCodeAt(0) < res.charCodeAt(0)) res = d[i]
+    }
+  }
+  return res
+
+  function findWord(word, string) {
+    let i, j
+    for (i = 0, j = 0; i < word.length && j < string.length; i++) {
+      if (word[i] == string[j]) j++
+      else {
+        j++
+        i--
+      }
+    }
+    if (j <= string.length && i == word.length) return true
+    return false
+  };
+}
