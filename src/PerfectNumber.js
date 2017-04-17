4108d1e0d30e8d95a251232628107a97c8e0bc26
Renascence <geniusglory@outlook.com>
2017-04-17 22:12:59 +0800

    Perfect Number

---
+++ src/PerfectNumber.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var checkPerfectNumber = function (num) {
+  if (num == 1 || num == 0) return false
+  let res = 0
+  const midNum = Math.sqrt(num)
+  for (let i = 1; i <= midNum; i++) {
+    if (num % i == 0) {
+      res += i
+      res += num / i
+    }
+  }
+  return res == num * 2
+};
