19d77fa8681ce1647848c76fe61a860d36759130
Renascence <geniusglory@outlook.com>
2017-04-20 22:57:11 +0800

    Complex Number Multiplication

---
+++ src/ComplexNumberMultiplication.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var complexNumberMultiply = function (a, b) {
+  const A = parseInt(a.split('+')[0])
+  const B = parseInt(a.split('+')[1])
+  const C = parseInt(b.split('+')[0])
+  const D = parseInt(b.split('+')[1])
+  return `${(A * C - B * D)}+${(A * D + B * C)}i`
+};
