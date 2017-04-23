f0883f67c712f1fabd16c0f547fd6b00a43417f5
Renascence <geniusglory@outlook.com>
2017-04-23 23:46:25 +0800

    Friend Circles

---
+++ src/FriendCircles.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[][]} M
+ * @return {number}
+ */
+
+var findCircleNum = function (M) {
+  const len = M.length
+  const visit = []
+  let res = 0
+  for(let i = 0;i<len;i++) {
+    if(visit[i]) continue
+    map(i)
+    res ++ 
+  }
+  return res
+
+  function map (k) {
+    visit[k] = true
+    for( let j = 0;j<len;j++) {
+      if(!M[k][j] || visit[j]) continue
+      map(j)
+    }
+  }
+};
