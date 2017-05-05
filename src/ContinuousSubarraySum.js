35e41ab8b06a975d18eeb2aaddfc147c7341b2b3
Renascence <geniusglory@outlook.com>
2017-05-05 23:13:30 +0800

    Continuous Subarray Sum

---
+++ src/ContinuousSubarraySum.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var checkSubarraySum = function (nums, k) {
+  let sum = 0
+  let remainder = {}
+  pre = 0
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i]
+    let temp = (k == 0) ? sum : sum % k
+    if (remainder[temp]) {
+      return true
+    }
+    remainder[pre] = 1
+    pre = temp
+  }
+  return false
+};
