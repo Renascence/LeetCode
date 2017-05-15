25cdd89548655620b33c6dcf9f38ddc4f595cd69
Renascence <geniusglory@outlook.com>
2017-05-15 20:40:03 +0800

    Shortest Unsorted Continuous Subarray

---
+++ src/ShortestUnsortedContinuousSubarray.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findUnsortedSubarray = function (nums) {
+  const temp = nums.slice(0)
+  temp.sort((a, b) => {
+    return a - b
+  })
+  let start = 0
+  let end = nums.length
+  for (let i = 0; i < temp.length; i++) {
+    if (temp[i] != nums[i]) {
+      start = i
+      break
+    }
+  }
+  for (let i = temp.length - 1; i >= 0; i--) {
+    if (temp[i] != nums[i]) {
+      end = i
+      break
+    }
+  }
+  if (start == 0 && end == nums.length) return 0
+  
+  return end - start + 1
+};
