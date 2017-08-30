2a22c0fea9220b2125fe91eb8db4b69f54033401
logging <logging.sheng@eku001.com>
2017-08-30 10:32:59 +0800

    Judge Route Circle

---
+++ src/JudgeRouteCircle.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} moves
+ * @return {boolean}
+ */
+var judgeCircle = function (moves) {
+  const moveObj = {
+    L: 0,
+    R: 0,
+    U: 0,
+    D: 0,
+  }
+  moves.split('').forEach((move) => {
+    moveObj[move]++
+  })
+  return moveObj.L === moveObj.R && moveObj.U === moveObj.D
+};
