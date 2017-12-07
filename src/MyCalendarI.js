337a16660bf05d4c9cfd87758898840752ae22c0
logging <logging.sheng@eku001.com>
2017-12-07 19:16:39 +0800

    My Calendar I

---
+++ src/MyCalendarI.js
@@ -0,0 +1,21 @@
+
+var MyCalendar = function () {
+  this.order = []
+};
+
+/** 
+* @param {number} start 
+* @param {number} end
+* @return {boolean}
+*/
+MyCalendar.prototype.book = function (start, end) {
+  var res = this.order.every(item => Math.max(item[0], start) >= Math.min(end, item[1]))
+  if (res) this.order.push([start, end])
+  return res
+};
+
+/** 
+* Your MyCalendar object will be instantiated and called as such:
+* var obj = Object.create(MyCalendar).createNew()
+* var param_1 = obj.book(start,end)
+*/
