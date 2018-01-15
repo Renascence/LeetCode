6d15ba2aa0013382fcada0a5a44d95529144ae45
logging <logging.sheng@eku001.com>
2018-01-15 11:09:58 +0800

    1-bit and 2-bit Characters

---
+++ src/1-bitand2-bitCharacters.js
@@ -0,0 +1,12 @@
+/**
+* @param {number[]} bits
+* @return {boolean}
+*/
+var isOneBitCharacter = function (bits) {
+  const len = bits.length;
+  let i = 0;
+  while (i < len-1) {
+    i += bits[i] + 1
+  }
+  return i === len - 1
+};
