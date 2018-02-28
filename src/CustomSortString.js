8e291418a75f1d924ec52e459b0c77d13d9a8ab3
logging <logging.sheng@eku001.com>
2018-02-28 16:05:36 +0800

    Custom Sort String

---
+++ src/CustomSortString.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} S
+ * @param {string} T
+ * @return {string}
+ */
+var customSortString = function (S, T) {
+  const len = S.length;
+  const map = {}
+  for (let i = 0; i < len; i++) {
+    map[S[i]] = i
+  }
+  return T.split('').sort((a, b) => {
+    if (map[b] === undefined) {
+      map[b] = 0
+    }
+    if (map[a] === undefined) {
+      map[a] = 0
+    }
+    return map[a] - map[b]
+  }).join('')
+};
