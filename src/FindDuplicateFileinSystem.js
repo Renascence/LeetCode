189e4181d80de68fb7fb4444fae98a6cbfd891dc
logging <geniusglory@outlook.com>
2018-03-25 22:39:16 +0800

    Find Duplicate File in System

---
+++ src/FindDuplicateFileinSystem.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string[]} paths
+ * @return {string[][]}
+ */
+var findDuplicate = function (paths) {
+  const reg = new RegExp("\\((.| )+?\\)", "igm");
+  const hash = {}
+  paths.forEach(item => {
+    const [path, ...file] = item.split(' ')
+    file.forEach(i => {
+      const content = i.match(reg)[0];
+      const name = i.replace(content, '')
+      if (hash[content]) {
+        hash[content].push(`${path}/${name}`)
+      } else {
+        hash[content] = [`${path}/${name}`]
+      }
+    })
+  })
+  const res = []
+  for (let i in hash) {
+    if (hash[i].length > 1) {
+      res.push(hash[i])
+    }
+  }
+  return res
+}
