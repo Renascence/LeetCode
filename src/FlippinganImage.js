d639c1c251dd921210e4d8fbec4949c4e0ce1956
logging <geniusglory@outlook.com>
2018-05-23 00:54:11 +0800

    Flipping an Image

---
+++ src/FlippinganImage.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[][]} A
+ * @return {number[][]}
+ */
+var flipAndInvertImage = function (A) {
+  return A.map(item => 
+    item.map(val => 1 - val)
+  ).map(item => item.reverse())
+};
