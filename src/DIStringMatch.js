efebcd042169397221cecc99b0b618d800a098f7
logging <geniusglory@outlook.com>
2019-03-05 22:11:01 +0800

    DI String Match

---
+++ src/DIStringMatch.js
@@ -0,0 +1,15 @@
+var diStringMatch = function(S) {
+  const len = S.length;
+  let min = 0;
+  let max = len;
+  const res = [];
+  for (let i = 0; i < len; i++) {
+    if (S[i] === 'I') {
+      res[i] = min++;
+    } else {
+      res[i] = max--;
+    }
+  }
+  res.push(min);
+  return res;
+};
