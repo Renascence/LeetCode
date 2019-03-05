d3bef208164447a6d45ff8ccd3d647be32c0dfcb
logging <geniusglory@outlook.com>
2019-03-05 22:09:04 +0800

    Sort Array By Parity

---
+++ src/SortArrayByParity.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} A
+ * @return {number[]}
+ */
+var sortArrayByParity = function (A) {
+  let res = [];
+  A.forEach(v => {
+    v % 2 === 0 ? res.unshift(v) : res.push(v);
+  })
+  return res;
+};
