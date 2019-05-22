92f9585d699ea4f90075befa6ec153f37f29c657
Renascence <geniusglory@outlook.com>
2019-05-23 01:06:00 +0800

    Fibonacci Numbe

---
+++ src/fibonacci-number.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} N
+ * @return {number}
+ */
+var fib = function (N) {
+  if (N == 0) return 0;
+  if (N == 1) return 1;
+  let res = [0 ,1]
+  let i = 2
+  while(i <= N) {
+    res[i] = res[i - 1] + res[i - 2]
+    i++
+  }
+  return res[N]
+};
