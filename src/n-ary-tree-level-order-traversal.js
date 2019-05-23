23515ebf700930b6a130add7d063e5ada77ff396
Renascence <geniusglory@outlook.com>
2019-05-24 01:01:08 +0800

    n-ary-tree-level-order-traversal

---
+++ src/n-ary-tree-level-order-traversal.js
@@ -0,0 +1,28 @@
+/**
+ * // Definition for a Node.
+ * function Node(val,children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+/**
+ * @param {Node} root
+ * @return {number[][]}
+ */
+var levelOrder = function (root) {
+  let res = [];
+
+  function loop(node, lv) {
+    if (!node) return
+    if (res[lv]) {
+      res[lv].push(node.val)
+    } else {
+      res[lv] = [node.val]
+    }
+    node.children.forEach(c => {
+      loop(c, lv + 1)
+    })
+  }
+  loop(root, 0)
+  return res
+};
