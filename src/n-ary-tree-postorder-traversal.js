5afcdf097f3d02e0003c648d8279eae554a6d194
Renascence <geniusglory@outlook.com>
2019-05-29 01:04:32 +0800

    n-ary-tree-postorder-traversal

---
+++ src/n-ary-tree-postorder-traversal.js
@@ -0,0 +1,23 @@
+/**
+ * // Definition for a Node.
+ * function Node(val,children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+/**
+ * @param {Node} root
+ * @return {number[]}
+ */
+var postorder = function (root) {
+  let res = [];
+  function loop(node) {
+    if (!node) return
+    node.children.forEach(c => {
+      loop(c)
+    })
+    res.push(node.val)
+  }
+  loop(root)
+  return res
+};
