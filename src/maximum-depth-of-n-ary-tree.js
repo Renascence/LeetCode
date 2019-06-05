92ef27af7c3e16d5cc8da0515e1ec413a533ca0a
Renascence <geniusglory@outlook.com>
2019-06-06 01:31:44 +0800

    maximum-depth-of-n-ary-tree

---
+++ src/maximum-depth-of-n-ary-tree.js
@@ -0,0 +1,22 @@
+/**
+ * // Definition for a Node.
+ * function Node(val,children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+/**
+ * @param {Node} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
+  let res = 0
+  function loop(root, lv) {
+    if(!root) return
+    res = lv > res ? lv : res
+    loop(root.left)
+    root.children.forEach(i => loop(i, lv + 1))
+  }
+  loop(root, 1)
+  return res
+};
