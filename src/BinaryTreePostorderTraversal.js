7a3ce2def25f62520190a30d2dbed5a4797fdce6
Renascence <geniusglory@outlook.com>
2019-07-20 09:38:22 +0800

    binary-tree-postorder-traversal

---
+++ src/BinaryTreePostorderTraversal.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function (root) {
+  const res = [];
+  function loop(node) {
+    if(!node) return
+    loop(node.left)
+    loop(node.right)
+    res.push(node.val)
+  }
+  loop(root)
+  console.log(res)
+  return res
+};
