9555e76448d334c65483383a20e24a2fdbf518f1
Renascence <geniusglory@outlook.com>
2019-07-28 15:40:22 +0800

    height-checker

---
+++ src/height-checker.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var heightChecker = function(heights) {
+  const sort = heights.slice().sort((a, b) => a-b);
+  let res = 0
+  sort.forEach((i, index) => {
+    if (i !== heights[index]) {
+      res ++
+    }
+  })
+  return res
+};
+
+
