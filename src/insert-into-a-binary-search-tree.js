46ea2a66d36d119ce94189e3b8b17b6a91ef47c5
Renascence <geniusglory@outlook.com>
2019-08-08 23:24:35 +0800

    insert-into-a-binary-search-tree

---
+++ src/insert-into-a-binary-search-tree.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var insertIntoBST = function (root, val) {
+  function loop(root) {
+    if (!root) return new TreeNode(val)
+    if (root.val > val) {
+      root.left = loop(root.left)
+    } else {
+      root.right = loop(root.right)
+    }
+    return root
+  }
+  loop(root)
+  return root
+};
