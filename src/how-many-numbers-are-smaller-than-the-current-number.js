46471793b98a3b43f9cb9eaaa919357be0ff09bb
Renascence <geniusglory@outlook.com>
2020-03-21 15:38:07 +0800

    How Many Numbers Are Smaller Than the Current Number

---
+++ src/how-many-numbers-are-smaller-than-the-current-number.js
@@ -0,0 +1,20 @@
+var smallerNumbersThanCurrent = function (nums) {
+  const temp = {}
+  nums.forEach(val => {
+    if (temp[val]) {
+      temp[val]++
+    } else {
+      temp[val] = 1
+    }
+  })
+  const keys = Object.keys(temp).sort((a, b) => (+a) - (+b))
+  const times = {}
+  keys.forEach((key, index) => {
+    if (index === 0) {
+      times[key] = 0
+    } else {
+      times[key] = temp[keys[index - 1]] + times[keys[index - 1]]
+    }
+  })
+  return nums.map(val => times[val])
+};
