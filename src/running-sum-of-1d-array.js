aaaf33603796e08575994e8d0caacc5dd1fa5efd
Renascence <geniusglory@outlook.com>
2020-07-28 17:29:32 +0800

    running-sum-of-1d-array

---
+++ src/running-sum-of-1d-array.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var runningSum = function (nums) {
+  nums.forEach((i, index) => {
+    if (index === 0) return
+    nums[index] = i + nums[index - 1]
+  })
+  return nums
+};
