7db503ef9ccfa0f761a420a6c5c82ff6ca62a29d
Renascence <geniusglory@outlook.com>
2020-11-11 20:37:48 +0800

    Find a Corresponding Node of a Binary Tree in a Clone of That Tree

---
+++ src/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} original
+ * @param {TreeNode} cloned
+ * @param {TreeNode} target
+ * @return {TreeNode}
+ */
+
+var getTargetCopy = function(original, cloned, target) {
+  let res = null
+  const bfs = node => {
+    if (node.val === target.val) {
+      res = node
+    } else {
+      if (node.left) {
+        bfs(node.left)
+      }
+      if (node.right) {
+        bfs(node.right)
+      }
+    }
+  }
+  bfs(cloned)
+  return res
+};
