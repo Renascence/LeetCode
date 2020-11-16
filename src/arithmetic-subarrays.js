a85b16e7bc736ddb93727c761ee0d68d390b22cb
Renascence <geniusglory@outlook.com>
2020-11-16 10:57:55 +0800

    Arithmetic Subarrays

---
+++ src/arithmetic-subarrays.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number[]} l
+ * @param {number[]} r
+ * @return {boolean[]}
+ */
+var checkArithmeticSubarrays = function (nums, l, r) {
+  const res = []
+  for (let i = 0; i < l.length; i++) {
+    const arr = nums.slice(l[i], r[i] + 1)
+    arr.sort((a, b) => a - b)
+    const minus = arr[1] - arr[0]
+    let flag = true
+    for (let j = 1; j < arr.length; j++) {
+      if (arr[j] - arr[j - 1] !== minus) {
+        res.push(false)
+        flag = false
+        break
+      }
+    }
+    if (flag) {
+      res.push(true)
+    }
+  }
+  return res
+};
